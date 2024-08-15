81563a1021ada7dd5c07e4e7801d3636931a5f96
Kaique <yannickaique@outlook.com>
2024-08-15 08:54:59 -0300

    Primeiro commit

---
+++ Niveis de acesso/Ponto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Niveis_de_acesso
+{
+    internal class Ponto
+    {
+        public double x;
+        public double y;
+
+        public Ponto(double x1, double y1)
+        {
+            x = x1;
+            y = y1;
+        }
+        public double Distancia(Ponto p)
+        {
+            return Math.Sqrt((x - p.x) * (x - p.x) + (y - p.y) * (y-p.y));
+        }
+    }
+}
---
+++ Niveis de acesso/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Niveis_de_acesso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ponto p1 = new Ponto(10, 20);
+            Ponto p2 = new Ponto(14, 12);
+
+            Console.WriteLine("Distancia entre (p1, p2) = {0}", p1.Distancia(p2));
+            Console.ReadKey(); 
+        }
+    }
+}
